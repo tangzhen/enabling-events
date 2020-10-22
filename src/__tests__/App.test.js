67e590a90a55641849914d0604e8f73797acb03d
Tang Zhen <ztang@thoughtworks.com>
2020-10-22 15:57:27 +0800

    [Zhen] fix the app unit test, because right now render the app header.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App.tsx';
 
-test('renders learn react link', () => {
+test('renders header create event button', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/CREATE EVENT/i);
   expect(linkElement).toBeInTheDocument();
 });
