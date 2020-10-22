d7cdea0f09095de9defb33a0e1c32778567e4bbb
Tang Zhen <ztang@thoughtworks.com>
2020-10-22 17:58:34 +0800

    [Zhen] add load more button.

--- src/components/HomeEventList.tsx
+++ src/components/HomeEventList.tsx
@@ -1,19 +1,48 @@
-import React from "react";
+import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import React from "react";
 import HomeEventItem from "./HomeEventItem";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    more: {
+      margin: theme.spacing(4),
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    moreButton: {
+      width: "300px",
+    },
+  })
+);
+
 export default function HomeEventList() {
+  const classes = useStyles();
+
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={4}>
-        <HomeEventItem />
+    <>
+      <Grid container spacing={3}>
+        <Grid item xs={4}>
+          <HomeEventItem />
+        </Grid>
+        <Grid item xs={4}>
+          <HomeEventItem />
+        </Grid>
+        <Grid item xs={4}>
+          <HomeEventItem />
+        </Grid>
       </Grid>
-      <Grid item xs={4}>
-        <HomeEventItem />
-      </Grid>
-      <Grid item xs={4}>
-        <HomeEventItem />
-      </Grid>
-    </Grid>
+      <div className={classes.more}>
+        <Button
+          variant="outlined"
+          color="primary"
+          className={classes.moreButton}
+        >
+          SEE MORE
+        </Button>
+      </div>
+    </>
   );
 }
