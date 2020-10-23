3c38df3d832c05554766f0de453cbdabce1fe1e0
Tang Zhen <ztang@thoughtworks.com>
2020-10-23 16:55:23 +0800

    [Zhen] update the json-server rewrite.

--- scripts/serve.js
+++ scripts/serve.js
@@ -7,11 +7,15 @@ const express = require("express");
 
 server.use(express.static(path.join(__dirname, "../build")));
 server.use("/files", express.static(path.join(__dirname, "../static/files")));
-
 server.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
 server.use(middlewares);
 server.use(router);
 
